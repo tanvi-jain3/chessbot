287022e603382b943e7f0a0d0ccf46f467fbcf88
Tanvi Jain <tanvi004@e.ntu.edu.sg>
2025-08-11 11:51:28 +0800

    Add moving piece functionality

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -1,5 +1,7 @@
 import './Chessboard.css';
 import Tile from '../Tile/Tile'
+import React from 'react';
+import { isAbsolute } from 'path';
 
 const verticalAxis=[1,2,3,4,5,6,7,8]
 const horizontalAxis=['a','b','c','d','e','f','g','h']
@@ -34,6 +36,38 @@ for(let i=0;i<8;i++){
     pieces.push({image: "assets/images/pawn_w.png",x:i,y:1})
 }
 
+let activePiece:HTMLElement | null =null
+
+
+function grabPiece(e: React.MouseEvent){
+    const element=e.target as HTMLElement;
+    if(element.classList.contains("chess-piece")){
+        const x=e.clientX - 50;
+        const y=e.clientY - 50;
+        element.style.position="absolute";
+        element.style.left=`${x}px`;
+        element.style.top=`${y}px`;
+
+        activePiece=element
+    }
+}
+
+function movePiece(e: React.MouseEvent){
+    if(activePiece){
+        const x=e.clientX - 50;
+        const y=e.clientY - 50;
+        activePiece.style.position="absolute";
+        activePiece.style.left=`${x}px`;
+        activePiece.style.top=`${y}px`;
+    }
+    
+}
+
+function dropPiece(e: React.MouseEvent){
+    if(activePiece){
+        activePiece=null;
+    }
+}
 
 export default function Chessboard(){
     let board=[];
@@ -46,10 +80,21 @@ export default function Chessboard(){
                 if(p.x==i && p.y==j){
                     image=p.image
                 }
-            })
+            });
 
-            board.push(<Tile image= {image} number={number}/>)
+
+
+            board.push(<Tile key={`${j},${i}`} image= {image} number={number}/>)
         }
     }
-    return <div id="chessboard">{board}</div>
+    return (
+    <div 
+    onMouseMove={(e) => movePiece(e)} 
+    onMouseDown={(e) => grabPiece(e)} 
+    onMouseUp={(e) => dropPiece(e)}
+    
+    id="chessboard">
+        {board}
+    </div>
+    );
 }
