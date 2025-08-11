ca6cf2975347f451de939b8e3a836ffb5b4dc124
Tanvi Jain <tanvi004@e.ntu.edu.sg>
2025-08-11 10:33:14 +0800

    Update Chess Board Starting Position

--- chess-app/src/components/Chessboard/Chessboard.tsx
+++ chess-app/src/components/Chessboard/Chessboard.tsx
@@ -10,7 +10,21 @@ interface Piece{
     y:number;
 }
 
-const pieces: Piece[]=[]
+const pieces: Piece[]=[];
+
+for(let p=0;p<2;p++){
+    const type = (p==0) ? "b":"w";
+    const y= p==0 ? 7:0;
+
+    pieces.push({image: `assets/images/rook_${type}.png`,x:0,y})
+    pieces.push({image: `assets/images/rook_${type}.png`,x:7,y})
+    pieces.push({image: `assets/images/knight_${type}.png`,x:1,y})
+    pieces.push({image: `assets/images/knight_${type}.png`,x:6,y})
+    pieces.push({image: `assets/images/bishop_${type}.png`,x:2,y})
+    pieces.push({image: `assets/images/bishop_${type}.png`,x:5,y})
+    pieces.push({image: `assets/images/queen_${type}.png`,x:3,y})
+    pieces.push({image: `assets/images/king_${type}.png`,x:4,y})
+}
 
 for(let i=0;i<8;i++){
     pieces.push({image: "assets/images/pawn_b.png",x:i,y:6})
@@ -19,24 +33,6 @@ for(let i=0;i<8;i++){
 for(let i=0;i<8;i++){
     pieces.push({image: "assets/images/pawn_w.png",x:i,y:1})
 }
-
-pieces.push({image: "assets/images/rook_b.png",x:0,y:7})
-pieces.push({image: "assets/images/rook_b.png",x:7,y:7})
-pieces.push({image: "assets/images/knight_b.png",x:1,y:7})
-pieces.push({image: "assets/images/knight_b.png",x:6,y:7})
-pieces.push({image: "assets/images/bishop_b.png",x:2,y:7})
-pieces.push({image: "assets/images/bishop_b.png",x:5,y:7})
-pieces.push({image: "assets/images/queen_b.png",x:3,y:7})
-pieces.push({image: "assets/images/king_b.png",x:4,y:7})
-
-pieces.push({image: "assets/images/rook_w.png",x:0,y:0})
-pieces.push({image: "assets/images/rook_w.png",x:7,y:0})
-pieces.push({image: "assets/images/knight_w.png",x:1,y:0})
-pieces.push({image: "assets/images/knight_w.png",x:6,y:0})
-pieces.push({image: "assets/images/bishop_w.png",x:2,y:0})
-pieces.push({image: "assets/images/bishop_w.png",x:5,y:0})
-pieces.push({image: "assets/images/queen_w.png",x:3,y:0})
-pieces.push({image: "assets/images/king_w.png",x:4,y:0})
 
 
 export default function Chessboard(){
